e50896a57fc3c5ba55a8211df9ed56dd301c39c8
PauuMendoza <pmendozac5@gmail.com>
2019-07-13 19:06:17 -0500

    fourth commit

---
+++ src/pkg1/operaciones/Ejercicio12_NumerosDivisibles.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1.operaciones;
+
+/**
+ *
+ * @author Paulina
+ * 12) Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3.
+ * Utiliza el bucle que desees.
+ */
+public class Ejercicio12_NumerosDivisibles {
+    
+    public static void main(String[] args) {
+        
+         int numeros=0;
+         
+        while(true){
+              numeros++;
+              if(numeros==100){
+             numeros %=2;
+                  System.out.println(numeros);
+            
+              }
+        }
+          
+            }
+        } 
+        
+        
+    
+    
+
+  
+  
